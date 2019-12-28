3a4bed879a30683472b03c0a747e3344fe73bbcf
Dawid <dyrcu15@gmail.com>
2019-12-28 17:54:41 +0100

    Added sending test notification via Firebase

---
+++ CMS.Notifications.Host/Program.cs
@@ -0,0 +1,33 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+
+namespace CMS.Notifications.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.GetApplicationDefault(),
+            });
+
+            var message = new Message()
+            {
+                Notification = new Notification
+                {
+                    Title = "Test title",
+                    Body = "Test title"
+                },
+                Topic = "main"
+            };
+
+            string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+
+            Console.WriteLine("Successfully sent message: " + response);
+            Console.ReadKey();
+        }
+    }
+}
